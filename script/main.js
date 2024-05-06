13351dff4fe8345d33baa6f3d799b1f3753a1ddc
Ahmed Eid <Ahmed_tech67@outlook.com>
2024-05-07 02:55:34 +0300

    Adding the active class to the menu

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-function loadPage(page) {
+function loadPage(page, link) {
   fetch(page)
     .then((response) => {
       if (!response.ok) {
@@ -8,13 +8,25 @@ function loadPage(page) {
     })
     .then((html) => {
       document.getElementById("main-content").innerHTML = html;
+      setActiveLink(link);
     })
     .catch((error) => {
       console.error("Error loading page:", error);
     });
 }
 
+// Function to set the active link
+function setActiveLink(clickedLink) {
+  const navLinks = document.querySelectorAll("nav a");
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+  });
+
+  clickedLink.classList.add("active");
+}
+
 // Load the default page when the page initially loads
 window.onload = function () {
-  loadPage("home.html"); // Set 'index.html' as the default page
+  const defaultLink = document.getElementById("homeLink");
+  loadPage("home.html", defaultLink);
 };
