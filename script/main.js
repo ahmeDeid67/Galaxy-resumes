f0c0c47540b2dfd6cb3d7ad097d094e5fe5e67cc
Ahmed Eid <Ahmed_tech67@outlook.com>
2024-05-07 07:20:53 +0300

    Add samples

--- script/main.js
+++ script/main.js
@@ -30,3 +30,19 @@ window.onload = function () {
   const defaultLink = document.getElementById("homeLink");
   loadPage("home.html", defaultLink);
 };
+
+// Function to open the modal
+function openModal(imageSrc, title) {
+  let modal = document.getElementById("myModal");
+  let modalImg = document.getElementById("modalImage");
+  let captionText = document.getElementById("caption");
+  modal.style.display = "block";
+  modalImg.src = imageSrc;
+  captionText.innerHTML = title;
+}
+
+// Function to close the modal
+function closeModal() {
+  let modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
